1707cdad0bb80e5b50a780866e14ac90981c81b4
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-01-14 10:19:33 +0530

    Node class completed

---
+++ src/LinkedLists/SinglyLinkedLists.java
@@ -0,0 +1,14 @@
+package LinkedLists;
+
+public class SinglyLinkedLists<E> {
+    private ListNode<E> head;
+
+
+
+    private static class ListNode<E>{
+        private E data;
+        private ListNode<E> next;
+    }
+}
+
+
