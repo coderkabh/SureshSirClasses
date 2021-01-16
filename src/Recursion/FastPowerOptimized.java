05c4cef783e10ee63c7b9486ef9839b37744077b
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-01-16 19:33:44 +0530

    completed class FastPowerOptimized.java

--- src/Recursion/FastPowerOptimized.java
+++ src/Recursion/FastPowerOptimized.java
@@ -1,4 +1,15 @@
 package Recursion;
 
 public class FastPowerOptimized {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        System.out.println(powerOpti(a,b));
+    }
+    static int powerOpti(int a, int b){
+        if(b == 0) return 1;
+        if(b % 2 == 0){
+            return powerOpti(a*a, b/2);
+        }
+        return a*powerOpti(a, b-1);
+    }
 }
