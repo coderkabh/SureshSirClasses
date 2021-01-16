336febe8c00a3dcaecfbdb5f9494a8e12f1a52a1
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-01-16 13:45:07 +0530

    made methods static of class SolvePermutation.java

--- src/Recursion/SolvePermutation.java
+++ src/Recursion/SolvePermutation.java
@@ -1,25 +1,22 @@
 package Recursion;
 
-import jdk.jfr.Percentage;
-
 public class SolvePermutation {
     public static void main(String[] args) {
         String str = "ABC";
         int n = str.length();
-        SolvePermutation sp = new SolvePermutation();
-        sp.permute(str,0 , n-1);
+        permute(str,0 , n-1);
     }
-    private void permute(String str, int l, int r){
+    static private void permute(String str, int l, int r){
         if(l == r){
             System.out.println(str);
         }
         for(int i=l; i<=r; i++){
-            str = swap(str, l, r);
+            str = swap(str, l, i);
             permute(str, l+1, r);
             str = swap(str, l ,i);
         }
     }
-    private String swap(String str, int i, int j){
+    static private String swap(String str, int i, int j){
         char temp;
         char[] charArray = str.toCharArray();
         temp = charArray[i];
