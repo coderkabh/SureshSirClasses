c2686e1ee2cebbe62207746bfa06ef31d3a6d699
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-01-16 19:01:25 +0530

    completed class SumOfArrayElements.java

--- src/Recursion/SumOfArrayElements.java
+++ src/Recursion/SumOfArrayElements.java
@@ -1,4 +1,20 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class SumOfArrayElements {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int sum = sumOfElements(arr,5);
+        System.out.println(sum);
+
+    }
+    private static int sumOfElements(int[] arr, int size){
+        if(size <= 0){
+            return 0;
+        }
+        else{
+            return (sumOfElements(arr,size-1) + arr[size-1]);
+        }
+    }
 }
