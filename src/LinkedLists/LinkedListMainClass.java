5414a5020916be443ce3adc565a4cfe69f87b27e
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2021-02-03 10:49:21 +0530

    completed the print method

---
+++ src/LinkedLists/LinkedListMainClass.java
@@ -0,0 +1,17 @@
+package LinkedLists;
+
+public class LinkedListMainClass {
+    public static void main(String[] args) {
+        SinglyLinkedLists sll = new SinglyLinkedLists();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode forth = new ListNode(11);
+
+        sll.head.next = second;
+        second.next = third;
+        third.next = forth;
+
+        sll.printLinkedList();
+    }
+}
--- src/LinkedLists/SinglyLinkedLists.java
+++ src/LinkedLists/SinglyLinkedLists.java
@@ -1,13 +1,30 @@
 package LinkedLists;
 
 public class SinglyLinkedLists<E> {
-    private ListNode<E> head;
+    ListNode<E> head;
     int counter = 0;
 
+    public void printLinkedList(){
+        ListNode<E> current = head;
+        while (current!=null){
+            if(current.next != null){
+                System.out.print(current.data + " -> ");
+            }
+            else{
+                System.out.print(current.data);
+            }
+            current = current.next;
+        }
+        System.out.println();
+    }
 
-    private static class ListNode<E>{
-        private E data;
-        private ListNode<E> next;
+}
+class ListNode<E>{
+    E data;
+    ListNode<E> next;
+    public ListNode(E data){
+        this.data = data;
+        this.next = null;
     }
 }
 
